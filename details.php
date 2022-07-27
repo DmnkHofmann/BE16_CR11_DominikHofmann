f62dffd3ade1a64ac96488210a0d48dd1c72c70c
DmnkHofmann <dmnkhofmann@gmail.com>
2022-07-27 15:58:09 +0200

    fixed

--- details.php
+++ details.php
@@ -13,6 +13,23 @@ $animal_sql = "SELECT * from animals WHERE animalid = $animal_id";
 $res = mysqli_query($connect, $animal_sql);
 $animal = mysqli_fetch_array($res, MYSQLI_ASSOC);
 
+$result = mysqli_query($connect, $animal_sql);
+$tbody = ''; //this variable will hold the body for the table
+if (mysqli_num_rows($result)  > 0) {
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        
+$tbody .= "
+
+<div class='card' style='width: 35rem;'>
+  <img src='pictures/" . $row['image'] . "'>
+  <div class='card-body'>
+    <p class='card-title'>".$row['short_description'] ."</p>
+  </div>
+</div>
+
+";
+    }
+};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,8 +39,17 @@ $animal = mysqli_fetch_array($res, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php require_once 'components/boot.php' ?>
+    <style>
+        .container{
+            height:100vh
+        }
+    </style>
 </head>
 <body>
-<?php echo $animal['short_description'] ?>
+
+<div class="container d-flex align-items-center justify-content-center">
+                <?= $tbody; ?>
+</div>
 </body>
 </html>
